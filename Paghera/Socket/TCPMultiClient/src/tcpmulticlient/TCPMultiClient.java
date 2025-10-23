5ced4393d50759a606dec3af7f0e716c4e5768f9
Fil-School <corsini.f@cerebotani.it>
2025-10-23 13:27:23 +0200

    TCP GIOCHINO BELLO

---
+++ Paghera/Socket/TCPMultiClient/src/tcpmulticlient/TCPMultiClient.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcpmulticlient;
+
+//Importo i package necessari
+import java.net.*;
+import java.io.*;
+   public class TCPMultiClient {
+     public void start()throws IOException {
+       //Connessione della Socket con il Server
+       Socket socket = new Socket("localhost", 7777);
+       //Stream di byte da passare al Socket
+       DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+       DataInputStream is = new DataInputStream(socket.getInputStream());
+       BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+       System.out.print("Per disconnettersi dal Server scrivere: QUIT\n");
+       //Ciclo infinito per inserimento testo del Client
+       while (true) {
+         System.out.print("Inserisci: ");
+         String userInput = stdIn.readLine();
+         if (userInput.equals("QUIT"))
+           break;
+         os.writeBytes(userInput + '\n');
+         System.out.println(is.readLine());
+         System.out.println(is.readLine());
+       }
+       //Chiusura dello Stream e del Socket
+       os.close();
+       is.close();
+       socket.close();
+     }
+     public static void main (String[] args) throws Exception {
+       TCPMultiClient tcpClient = new TCPMultiClient();
+       tcpClient.start();
+     }
+   }
---
+++ Paghera/Socket/TCPMultiServer/src/tcpmultiserver/TCPMultiServer.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcpmultiserver;
+
+import java.net.*;
+import java.io.*;
+import java.util.Random;
+   //Creazione di una classe per il Multrithreading
+   class ServerThread extends Thread {
+     private Socket socket;
+     public ServerThread (Socket socket) {
+       this.socket = socket;
+     }
+     //esecuzione del Thread sul Socket
+     public void run() {
+        Random rand = new Random();
+        int count = 1;
+
+        int numeroLotto = rand.nextInt(90); // Numero tra 0 e 99
+        System.out.println(numeroLotto);
+        
+       try {
+         DataInputStream is = new DataInputStream(socket.getInputStream());
+         DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+         while(true) {
+           String userInput = is.readLine();
+           if (userInput == null || userInput.equals("QUIT"))
+             break;
+           
+           
+           if (numeroLotto > Integer.parseInt(userInput)) {
+               os.writeBytes("Errato! Il numero ");
+               os.writeBytes(userInput + " e' troppo basso" + '\n');
+               os.writeBytes("Hai effettuato " + count + " tentativi/o" + '\n');
+               count++;
+           } else if (numeroLotto < Integer.parseInt(userInput)){
+               os.writeBytes("Errato! Il numero ");
+               os.writeBytes(userInput + " e' troppo alto" + '\n');
+               os.writeBytes("Hai effettuato " + count + " tentativi/o" + '\n');
+               count++;
+           } else {
+               os.writeBytes("Corretto!!" + '\n');
+               os.writeBytes("Hai terminato il gioco con " + count + " tentativi/o" + '\n');
+               count++;
+           }
+           
+           System.out.println("Il Client ha scritto: " + userInput);
+         }
+         os.close();
+         is.close();
+         System.out.println("Ho ricevuto una chiamata di chiusura da:\n" + socket +
+"\n");
+         socket.close();
+       }
+       catch (IOException e) {
+         System.out.println("IOException: " + e);
+       }
+     }
+   }
+   //Classe Server per attivare la Socket
+   public class TCPMultiServer {
+     public void start() throws Exception {
+       ServerSocket serverSocket = new ServerSocket(7777);
+       //Ciclo infinito di ascolto dei Client
+       while(true) {
+         System.out.println("In attesa di chiamate dai Client... ");
+         Socket socket = serverSocket.accept();
+         System.out.println("Ho ricevuto una chiamata di apertura da:\n" + socket);
+         ServerThread serverThread = new ServerThread(socket);
+         //Il metodo che permette la gestione del servizio sull'oggetto appena creato
+		 serverThread.start();
+       }
+     }
+     public static void main (String[] args) throws Exception {
+       TCPMultiServer  tcpServer = new TCPMultiServer ();
+      tcpServer.start();
+     }
+   }
